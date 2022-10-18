ec34e4a349ab22da8d0eebfbfdabc566c3e5a915
hsm1020s <111288446+hsm1020s@users.noreply.github.com>
2022-10-18 21:09:37 +0900

    [Silver V] Title: 소수 찾기, Time: 216 ms, Memory: 17692 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.java
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.java
@@ -7,17 +7,17 @@ public class Main {
         int num=0,so = 0,tmp=0;
         
         for(int i=0; i<N; i++){ // 몇개 비교할건지
-            num = in.nextInt(); 
+            num = in.nextInt(); //숫자 하나씩 입력받기
             
-            if(num==1)
+            if(num==1) //1일경우 위로
             	continue;
-            for(int j=2;j<num;j++) {
-            	if((num%j)==0) {
-            		tmp=1;
+            for(int j=2;j<num;j++) { 
+            	if((num%j)==0) { //소수 아닐경우
+            		tmp=1; 
             		break;
             	}
             }
-            if(tmp==0) {
+            if(tmp==0) { //소수
             	so++;
             }
             tmp=0;
